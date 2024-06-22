bb42a3239a26586e7684fa5ed128466c6ac195e4
Divyansh3021 <divyansh3021@gmail.com>
2024-06-22 22:10:21 +0530

    working section complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
+import Working from "@/components/Working";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Hero/>
       <Features/>
+      <Working/>
     </main>
   );
 }
---
+++ src/components/Working.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { BackgroundBeams } from "./ui/background-beams";
+import update from "@/images/update.png"
+import fetch from "@/images/fetch.png"
+
+export default function Working() {
+    return(
+        <div className="mt-20 flex flex-col align-middle justify-center">
+            <BackgroundBeams/>
+            <h2 className="text-lg md:text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-20">Usage</h2>
+            <Image src={update} alt="update" className=" max-w-5xl max-h-3xl mt-3"/>
+            <Image src={fetch} alt="fetch" className=" max-w-5xl max-h-3xl mt-3"/>
+        </div>
+    )
+}
