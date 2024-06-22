c423047c9589d8ef265b6684bae0234e2e0ed7d1
Divyansh3021 <divyansh3021@gmail.com>
2024-06-22 21:29:42 +0530

    features page

---
+++ src/components/Features.tsx
@@ -0,0 +1,74 @@
+import { TracingBeam } from "./ui/tracing-beam";
+import { twMerge } from "tailwind-merge";
+
+
+export default function Features() {
+    const dummyContent = [
+        {
+          title: "Effortless Code Search",
+          description: (
+            <>
+              <p>
+              Quickly find and retrieve code snippets from your previous projects without leaving VS Code. Save time and focus on what matters most—coding.
+              </p>
+            </>
+          ),
+        },
+        {
+          title: "Customizable Integrations",
+          description: (
+            <>
+              <p>
+              Tailor Code Dog to fit your unique coding style and project requirements. Adapt past solutions seamlessly to meet your current needs.
+              </p>
+            </>
+          ),
+        },
+        {
+          title: "Boosted Productivity",
+          description: (
+            <>
+              <p>
+              Say goodbye to redundant coding and hello to a streamlined workflow. Code Dog enhances your coding speed and efficiency by reusing your best work.
+              </p>
+            </>
+          ),
+        },
+        {
+            title: "Seamless VS Code Integration",
+            description:(
+                <p>
+                    Enjoy a smooth and intuitive experience with Code Dog’s seamless integration into VS Code. It works right where you do, making it an essential part of your development toolkit.
+                </p>
+            )
+        }
+      ];
+
+    return (
+        <div>
+
+            <h2 className="text-lg md:text-4xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50 mt-20">
+              Features
+            </h2>
+
+            <TracingBeam className="px-5 mt-10">
+                <div className="max-w-2xl mx-auto antialiased pt-4 relative">
+                    {dummyContent.map((item, index) => (
+                        <div key={`content-${index}`} className="mb-10">        
+                        <p className={twMerge("text-xl mb-4")}>
+                        {item.title}
+                        </p>
+            
+                        <div className="text-sm  prose prose-sm dark:prose-invert">
+                        {item.description}
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            </TracingBeam>
+        </div>
+
+    )
+
+
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,29 +1,32 @@
 import Link from "next/link"
 import { Spotlight } from "./ui/Spotlight"
 import { Button } from "./ui/moving-border"
+import { HoverBorderGradient } from "./ui/hover-border-gradient"
 
 export default function Hero(){
+
     return (
         <div className="">
             <Spotlight
                 className="-top-40 left-0 md:left-60 md:-top-20"
                 fill="violet"
             />
+
             <h2 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
                 Meet Your New Best Friend in VS Code!
             </h2>
             <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-            Meet Code Dog, your ultimate VS Code companion. Fetch and integrate your past code effortlessly, boost productivity, and streamline your workflow. Say goodbye to redundant coding—let Code Dog lead the way! 🐶🚀
+            Meet Code Dog, your ultimate VS Code companion. Fetch and integrate your past code effortlessly, boost productivity, and streamline your workflow. Say goodbye to redundant coding—let Code Dog lead the way! 
             </p>
 
-            <Link href={"https://marketplace.visualstudio.com/items?itemName=Divyansh3021.CodeArchive"} target="_blank" className="flex justify-center align-middle mt-5">
-                <Button
-                    borderRadius="1.5rem"
-                    className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
-                >
-                    Add to VS Code
-                </Button>
+            <div className="mt-5 flex align-middle justify-center">
+
+            <Link href={"https://marketplace.visualstudio.com/items?itemName=Divyansh3021.CodeArchive"} target="_blank" >
+                <HoverBorderGradient containerClassName="rounded-full" as="button" className=" border-spacing-9 dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2">
+                    <span>Add to VS Code</span>
+                </HoverBorderGradient>
             </Link>
+            </div>
         </div>
     )
 }
