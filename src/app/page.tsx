98217ff975b3cb24af9257d75cf8977cf736dda3
Divyansh3021 <divyansh3021@gmail.com>
2024-07-01 08:24:17 +0530

    fetch image updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-6">
         <Hero/>
         <Features/>
-        <Working/>
         <Testimonials/>
+        <Working/>
         <Footer/>
       </main>
   );
